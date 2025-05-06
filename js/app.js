0f0142fff3cb2e5675d0f0887eceac0c818f2923
DianaArman1216 <dianmap96@icloud.com>
2025-05-05 22:48:36 -0400

    first CSS layout and board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-console.log("Hello World!");
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let turn = 'X';
+let board = [
+    "", "", "", "", "",
+    "", "", "", "", "",
+    "", "", "", "", "",
+    "", "", "", "", "",
+    "", "", "", "", "",
+  ]  
+  let isWinner 
+  let isTie
+
+/*------------------------ Cached Element References ------------------------*/
+const gameBoardEl = document.querySelector('.game-board')
+console.log(gameBoardEl)
+const allDots = []
+for (let i = 0; i < 25; i++) {
+    const dot = document.createElement('div')
+    dot.classList.add('dot')
+    dot.setAttribute('data-index', i)
+    allDots.push(dot)
+    gameBoardEl.appendChild(dot)
+}
+console.log(allDots)
+
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    board = [
+      "", "", "", "", "",
+      "", "", "", "", "",
+      "", "", "", "", "",
+      "", "", "", "", "",
+      "", "", "", "", "",
+    ]
+    turn = "X"
+    isWinner = false
+    isTie = false
+    render()
+  }
+  const render = () => {
+      console.log('render');
+  }
+  
+/*----------------------------- Event Listeners -----------------------------*/

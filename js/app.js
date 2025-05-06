dfdaa36676622b29c781a4837a0df4b85ce0cecb
DianaArman1216 <dianmap96@icloud.com>
2025-05-06 01:12:47 -0400

    need to debug

--- js/app.js
+++ js/app.js
@@ -54,4 +54,44 @@ const init = () => {
   }
   
 
+
+let selectedDots =[]
+allDots.forEach(dot =>{
+  dot.addEventListener('click', (e) =>{
+    const index = e.target.dataset.index
+    selectedDots.push(index)
+    dot.classList.add('clicked')
+   
+
+    if(selectedDots.length === 2) {
+      drawLine(selectedDots[0], selectedDots[1])
+      selectedDots = []
+      allDots.forEach(d => d.classList.remove('clicked'))
+    }
+  })
+})
+
+
+const drawLine = (index1, index2) => {
+  const dot1 = allDots[index1]
+  const dot2 = allDots[index2]
+
+  const rect1 = dot1.getBoundingClientRect()
+  const rect2 = dot2.getBoundingClientRect()
+  console.log(rect1, rect2)
+  const canvasRect = canvasElement.getBoundingClientRect()
+
+  const x1 = rect1.left + rect1.width / 2 - canvasRect.left
+  const y1 = rect1.top + rect1.height / 2 - canvasRect.top
+  const x2 = rect2.left + rect2.width / 2 - canvasRect.left
+  const y2 = rect2.top + rect2.height / 2 - canvasRect.top
+
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = 'red'
+  ctx.stroke()
+};
+
+
 /*----------------------------- Event Listeners -----------------------------*/
